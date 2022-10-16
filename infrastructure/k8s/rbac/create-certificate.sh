ab903fb294272e9f01e7fdbd4d6e236856b67a54
ryan-s-cloud <103324348+ryan-s-cloud@users.noreply.github.com>
2022-10-15 17:22:04 -0700

    Replace CSR in yaml with actual CSR

--- infrastructure/k8s/rbac/create-certificate.sh
+++ infrastructure/k8s/rbac/create-certificate.sh
@@ -1,2 +1,10 @@
+#!/bin/bash
+
+set -x
+
 openssl genrsa -out  capstone-project1-pod-user.key 2048
 openssl req -new -key capstone-project1-pod-user.key -out capstone-project1-pod-user.csr -subj "/CN=capstone-project1-pod-user/O=capstone-project1"
+
+# Replace the CSR in the csr.yaml
+CSR=$(cat capstone-project1-pod-user.csr | base64 | tr -d '\n')
+sed -i "s/__CSR___/${CSR}/g" capstone-project1-pod-user-csr.yaml

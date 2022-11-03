6e3d79fc9a483011e36f0ef4b392be3166cd3192
ryan-s-cloud <103324348+ryan-s-cloud@users.noreply.github.com>
2022-11-02 20:06:34 -0700

    Update create-pod-user-config.sh

--- infrastructure/k8s/rbac/create-pod-user-config.sh
+++ infrastructure/k8s/rbac/create-pod-user-config.sh
@@ -5,10 +5,13 @@ set -x
 user='capstone-project1-pod-user'
 namespace='capstone-project1'
 
+CCDATA='client-certificate-data:.*$'
+CKDATA='client-key-data:.*'
+
 sed s/kubernetes-admin/${user}/g ~/.kube/config |\
 sed s/@kubernetes/@${namespace}/g |\
-sed "s/client-certificate-data:.*$/client-certificate-data: $(kubectl get csr ${user} -o jsonpath={.status.certificate})/g" |\
-sed "s/client-key-data:.*$/client-key-data: $(cat ${user}.key | base64 | tr -d '\n')/g" \
+sed "s/${CCDATA}/client-certificate-data: $(kubectl get csr ${user} -o jsonpath={.status.certificate})/g" |\
+sed "s/${CKDATA}/client-key-data: $(cat ${user}.key | base64 | tr -d '\n')/g" \
   > ${user}.conf
 
 # permission denied
